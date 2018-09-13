661e534ef69b2db8c6f620da8fd7fc836a0e0a16
Rodolfo Arteaga Held <ro_arteaga@javeriana.edu.co>
2018-09-12 23:14:17 -0500

    1ra versión de campañas

---
+++ src/main/java/com/touresBalon/b2c/B2Cbusinesscampaigns/B2CBusinessCampaignsApplication.java
@@ -0,0 +1,12 @@
+package com.touresBalon.b2c.B2Cbusinesscampaigns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2CBusinessCampaignsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2CBusinessCampaignsApplication.class, args);
+	}
+}
